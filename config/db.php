bbf88eac8ce216c35aa378c520b2e01a4ddd83d6
Ephraim Baldado <ephraimbaldado@gmail.com>
2026-05-18 19:42:17 +0800

    DB config for database connection.

---
+++ config/db.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'host' => '127.0.0.1',
+    'port' => 3306,
+    'dbname' => 'jobseeker',
+    'username' => 'root',
+    'password' => '',
+];
+
+//Sir phpmyadmin po ginamit ko para sa database ko po
